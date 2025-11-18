ecb23137bccae44c73c9d39e6351c062e771af3c
Eshel Eyni <esheleyni@gmail.com>
2025-11-18 18:32:59 +0200

    added navgation function to product preview

--- src/pages/ProductDetails/components/ProductPreview.tsx
+++ src/pages/ProductDetails/components/ProductPreview.tsx
@@ -1,13 +1,20 @@
 import type { FC } from "react";
 import type { Product } from "../../../types/Product";
+import { useNavigate } from "react-router-dom";
 
 type ProductPreviewProps = {
   product: Product;
 };
 
 export const ProductPreview: FC<ProductPreviewProps> = ({ product }) => {
+  const navigate = useNavigate();
+
+  const onClickPreview = () => {
+    navigate(`/product/${product.id}`);
+  };
+
   return (
-    <article className="border-2 border-black">
+    <article className="border-2 border-black cursor-pointer" onClick={onClickPreview}>
       <img src={product.thumbnailUrl} alt={`${product.name} thumbnail`} />
       <div className="p-4">
         <h3>{`id: ${product.id}`}</h3>
